afd6030b4c95e9194f4fb7843f52ff1a8cf26b47
Guilherme Carvalho <162490839+guicarbar@users.noreply.github.com>
2025-03-30 00:25:55 -0300

    update estructura for round (#9)
    - add equalsGolpes
    - add diferentsGolpes

--- Fight/Modules/Fight.java
+++ Fight/Modules/Fight.java
@@ -11,15 +11,57 @@ public class Fight {
         this.enemy = enemy;
     }
 
+
+    // golpes iguais
+    private static void equalsGolpes(int golpe) {
+        switch (golpe) {
+            case 1:
+                // jab
+
+                break;
+            case 2:
+                // chute baixo
+
+                break;
+            case 3:
+                // cruzado
+
+                break;
+            case 4:
+                // chute alto
+
+                break;
+            case 5:
+                // empurrar
+
+                break;
+            case 6:
+                // defender
+
+                break;
+            default:
+                // ble
+
+                break;
+        }
+    }
+
+
+    // golpes diferentes
+    private static void diferentsGolpes(int playerGolpe, int enemyGolpe) {
+            
+    }
+
+
     // metods for fiht
     public static void comparGolpes(int playerGolpe, int enemyGolpe) {
         // verificador
         if (playerGolpe == enemyGolpe) {
             // para golpes iguais
-
+            equalsGolpes(playerGolpe);
         } else {
             // para golpes diferentes
-            
+            diferentsGolpes(playerGolpe, enemyGolpe);
         }
     }
 }
